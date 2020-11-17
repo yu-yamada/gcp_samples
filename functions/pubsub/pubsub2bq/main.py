9c2812fa75305f0992c3fb70ec2b80bb3534eb9f
U-yamada <yu.yamada07@gmail.com>
2020-11-17 11:21:14 +0900

    Update main.py

--- functions/pubsub/pubsub2bq/main.py
+++ functions/pubsub/pubsub2bq/main.py
@@ -1,5 +1,12 @@
 from google.cloud import bigquery
 import base64, sys, os
+import logging
+import google.cloud.logging
+client = google.cloud.logging.Client(project=os.environ['project_id'])
+handler = client.get_default_handler()
+cloud_logger = logging.getLogger('cloudLogger')
+cloud_logger.setLevel(logging.INFO)  # defaults to WARN
+cloud_logger.addHandler(handler)
 def pubsub_to_bq(event, context):
     pubsub_message = base64.b64decode(event['data']).decode('utf-8')
     print(pubsub_message)
@@ -9,6 +16,14 @@ def to_bigquery(dataset, table, column, document):
     dataset_ref = bigquery_client.dataset(dataset)
     table_ref = dataset_ref.table(table)
     table = bigquery_client.get_table(table_ref)
-    errors = bigquery_client.insert_rows(table, [{column:document}])
-    if errors != [] :
-        print(errors, file=sys.stderr)
+    try:
+        ret = bigquery_client.insert_rows(table, [{column:document}])
+        print(ret)
+        if  ret != [] :
+            cloud_logger.error(ret)
+            print(ret)
+            sys.exit(1)
+    except Exception as e:
+        cloud_logger.error(e)
+        print(ret)
+        sys.exit(1)
